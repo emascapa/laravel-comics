763ea2d37fa05fbaa13343b289417209cc9183fd
emascapa <emascapa@gmail.com>
2022-06-10 18:27:13 +0200

    add button on comics sections

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -12,7 +12,10 @@
 
 @section('content')
     <section id="comics_wrapper">
-        <div class="container py-5">
+        <div class="container py-5 position-relative">
+
+            <h3 class="fs-4 text-white bg-primary px-3 py-1 m-0 text-uppercase position-absolute top-0 start-0 translate-middle-y">current series</h3>
+
             <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-3">
 
                 @foreach (config('db.comics') as $index => $comic)
@@ -29,6 +32,9 @@
                     </div>
                 @endforeach
 
+            </div>
+            <div class="mt-3 py-2 text-center">
+                <span class="fs-6 text-white bg-primary px-3 py-1 text-uppercase">view more</span>
             </div>
         </div>
     </section>
