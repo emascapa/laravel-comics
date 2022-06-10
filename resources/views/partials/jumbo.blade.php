9f85f2283c67f48e2e720ece85e03b9db4fcb30d
emascapa <emascapa@gmail.com>
2022-06-10 18:18:33 +0200

    add jumbotron

--- resources/views/partials/jumbo.blade.php
+++ resources/views/partials/jumbo.blade.php
@@ -1,8 +1,3 @@
 <section>
-    <div class="p-5 bg-light">
-        <div class="container">
-            <h1 class="display-3">Jumbo heading</h1>
-            <p class="lead">Jumbo helper text</p>
-        </div>
-    </div>
+    <div class="jumbotron"></div>
 </section>
